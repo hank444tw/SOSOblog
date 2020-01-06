20a589c0f765078645d8c1ce684a6f73b992bea8
hank444tw <42534903+hank444tw@users.noreply.github.com>
2020-01-07 03:21:46 +0800

    Add files via upload

---
+++ Scripts/MyNavbar.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $(window).resize(function () {
+        changeNav();
+    });
+
+    function changeNav() {
+        //console.log($(window).width());
+        let w = $(window).width();
+        if (w < 992) {
+            $("#Navbar-lg").css("display", "none");
+            $("#Navbar-xs").css("display", "");
+        }
+        else {
+            $("#Navbar-lg").css("display", "");
+            $("#Navbar-xs").css("display", "none");
+        };
+    };
+
+    changeNav();
+});
---
+++ Scripts/index.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+
+    /*scoll滑動功能*/
+    $("#starticon").click(function () {
+        let target = $("#bigimg").height();
+        $("html,body").animate({ //瀏覽器緣故，故html和body兩個都選取
+            scrollTop: target
+        },800);
+    });
+
+    /*碰觸文章圖片，眼球會淡入淡出的效果*/
+    $(".article-img div").hover(function () {
+        //console.log($(this).children().eq(1));
+        $(this)
+            .children()
+            .eq(1)
+            .stop()
+            .fadeToggle();
+    });
+
+    /*進入列表按鈕，動畫效果*/
+    $(".golist-btn").mouseenter(function () {
+        $(".golist-btn-span").css("width", "100%");
+    }).mouseleave(function () {
+        $(".golist-btn-span").css("width", "0%");
+    });
+});
